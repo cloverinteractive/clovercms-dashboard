6cd694a8abd24261ac2110d0edb01e50c2148bc3
Enrique Vidal <enrique@cloverinteractive.com>
2011-06-13 23:34:31 -0700

    Started install generator

--- lib/generators/clover_cms/dashboard/install/install_generator.rb
+++ lib/generators/clover_cms/dashboard/install/install_generator.rb
@@ -3,13 +3,13 @@ require 'rails/generators/migration'
 module CloverCms
   module Dashboard
     module Generators
-      class InstallGenerator < ::Rails::Generators::Base
+      class InstallGenerator < Rails::Generators::Base
         source_root File.expand_path('../../templates', __FILE__)
         desc "Copies clovercms-dashboard sass to current project"
         argument :path, :required => false, :default => :sass, :desc => "The path inside app to put your sass"
 
-        def copy_sass
-          directory 'sass', "app/#{scope}"
+        def generate_sass
+          directory 'sass', "app/#{path || :sass}"
         end
       end
     end
